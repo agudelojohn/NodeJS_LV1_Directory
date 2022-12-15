29c7d6b3deca736b034909b2568c008bd69bb8b5
John Agudelo <jhbr08@hotmail.com>
2022-12-15 18:09:25 -0500

    Improvement to methods get

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -1,10 +1,33 @@
 const { populate } = require('./../utils/transformData');
 let books = populate(15);
 
-exports.getAllBooks = async () => {
-  return books;
+exports.getAllBooks = (req, res) => {
+  try {
+    res.status(200).json({
+      status: 'success',
+      data: books,
+    });
+  } catch (err) {
+    console.error(err);
+  }
 };
 
-exports.getById = async (id) => {
-  return books.find((el) => el.id === id);
+exports.getById = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const book = await books.find((el) => el.id === id);
+    if (book) {
+      res.status(200).json({
+        status: 'success',
+        data: book,
+      });
+    } else {
+      res.status(404).json({
+        status: 'fail',
+        message: 'Not found',
+      });
+    }
+  } catch (err) {
+    console.error(err);
+  }
 };
--- routes/booksRoutes.js
+++ routes/booksRoutes.js
@@ -2,29 +2,8 @@ const express = require('express');
 const router = express.Router();
 const booksController = require('./../controllers/booksController');
 
-router.route('/').get(async (req, res) => {
-  try {
-    const books = await booksController.getAllBooks();
-    res.status(200).json({
-      status: 'success',
-      data: books,
-    });
-  } catch (err) {
-    console.error(err);
-  }
-});
+router.route('/').get(booksController.getAllBooks);
 
-router.route('/:id').get(async (req, res) => {
-  try {
-    const id = req.params.id;
-    const book = await booksController.getById(id);
-    res.status(200).json({
-      status: 'success',
-      data: book,
-    });
-  } catch (err) {
-    console.error(err);
-  }
-});
+router.route('/:id').get(booksController.getById);
 
 module.exports = router;
